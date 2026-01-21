8822974127eb03ab0bf32356824509ae52b48c9e
Walace Ramos <contatowalace.dev@gmail.com>
2026-01-21 20:33:43 -0300

    estilização e banco de dados

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,8 @@
 import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma: any };
 
-const dbUrl = process.env.DATABASE_URL || "file:./dev.db";
-
-// @ts-ignore
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
