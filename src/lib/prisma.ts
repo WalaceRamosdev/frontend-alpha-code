08ae42d88b1e6a8b130a1d838ef9315a79e9d254
Walace Ramos <contatowalace.dev@gmail.com>
2026-01-22 12:24:21 -0300

    configurar prisma com directUrl e novas keys

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
-const globalForPrisma = globalThis as unknown as { prisma: any };
+const createPrismaClient = () => {
+    return new PrismaClient().$extends(withAccelerate());
+};
 
-export const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+const globalForPrisma = globalThis as unknown as {
+    prisma: ReturnType<typeof createPrismaClient> | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
