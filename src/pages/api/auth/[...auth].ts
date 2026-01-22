8af38096b7e8d86db48d5af362ea16dc55421742
Walace Ramos <contatowalace.dev@gmail.com>
2026-01-22 11:06:56 -0300

    remover rota auth duplicada - auth-astro já cria automaticamente

--- src/pages/api/auth/[...auth].ts
+++
@@ -1,7 +0,0 @@
-import { Auth } from "@auth/core";
-import type { APIRoute } from "astro";
-import authConfig from "../../../../auth.config";
-
-export const ALL: APIRoute = async ({ request }) => {
-    return Auth(request, authConfig) as Promise<Response>;
-};
