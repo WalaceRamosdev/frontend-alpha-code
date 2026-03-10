031f4e7c6b53afce0a4ee6cd38df57d0b8d3ec00
Walace Ramos <contatowalace.dev@gmail.com>
2026-03-10 16:17:57 -0300

    chore: optimize unused js and fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,18 @@ import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
 import auth from 'auth-astro';
 import tailwindcss from '@tailwindcss/vite';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.sitesalphacode.com.br',
   output: 'server',
   adapter: vercel(),
-  integrations: [react(), sitemap(), auth()], // Trigger deploy
+  integrations: [react(), sitemap(), auth(), partytown({
+    config: {
+      forward: ['dataLayer.push'],
+    },
+  })], // Trigger deploy
   build: {
     inlineStylesheets: 'always'
   },
